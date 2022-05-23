7eggs/node-toggl-api

commit d396c1cda5d5f54fc132a24e79c94f1dc76aff33

Updated api url

lib/client.js
@@ -13,8 +13,8 @@ var DEFAULT_INSTANCE = null;
 var DEFAULTS = {
   reauth: false,
   sessionCookie: 'toggl_api_session',
-  apiUrl: 'https://www.toggl.com',
-  reportsUrl: 'https://toggl.com/reports'
+  apiUrl: 'https://api.track.toggl.com',
+  reportsUrl: 'https://api.track.toggl.com/reports/api/v2'
 };
 
 
@@ -102,8 +102,7 @@ TogglClient.prototype.apiRequest = function apiRequest(path, opts, callback) {
     });
 
     return;
-  }
-  else if (!opts.noauth && !options.apiToken && !this.authData) {
+  } else if (!opts.noauth && !options.apiToken && !this.authData) {
     return callback(new Error('Authenticate first'));
   }
 
@@ -113,8 +112,7 @@ TogglClient.prototype.apiRequest = function apiRequest(path, opts, callback) {
         user: options.apiToken,
         pass: 'api_token'
       };
-    }
-    else {
+    } else {
       opts.jar = this.cookieJar;
     }
   }
@@ -133,8 +131,7 @@ TogglClient.prototype.apiRequest = function apiRequest(path, opts, callback) {
 
     if (statusCode >= 200 && statusCode < 300) {
       callback(null, data);
-    }
-    else {
+    } else {
       callback(new APIError(statusCode, data));
     }
   }
@@ -159,8 +156,7 @@ TogglClient.prototype.authenticate = function authenticate(callback) {
       user: options.username,
       pass: options.password
     };
-  }
-  else {
+  } else {
     return callback(new Error('No need to authenticate thus you use apiToken'));
   }
 
@@ -181,8 +177,7 @@ TogglClient.prototype.authenticate = function authenticate(callback) {
 
     if (options.reauth) {
       self.cookieJar._jar.getCookies(options.apiUrl, oncookies);
-    }
-    else {
+    } else {
       success();
     }
   }
@@ -239,8 +234,7 @@ function findCookieByKey(cookies, key) {
 }
 
 
-function noop() {
-}
+function noop() {}
 
 
 /**
@@ -278,11 +272,9 @@ TogglClient.prototype.reportsRequest = function reportsRequest(path, opts,
 
     if (statusCode >= 200 && statusCode < 300) {
       callback(null, data);
-    }
-    else if (data.error) {
+    } else if (data.error) {
       callback(new ReportError(data.error));
-    }
-    else {
+    } else {
       callback(new ReportError('Unknown Reports API error', statusCode, data));
     }
   }

commit e9f5eaba8486744c2b12cfab2dc476c429e1c912

Remove version path from reportsUrl
This commit https://github.com/7eggs/node-toggl-api/commit/d396c1cda5d5f54fc132a24e79c94f1dc76aff33 broke the reports endpoints because the `/api/v2` is added with every `reportsRequest` call. This results in invalid calls to the reports API (404 not found)

lib/client.js
@@ -14,7 +14,7 @@ var DEFAULTS = {
   reauth: false,
   sessionCookie: 'toggl_api_session',
   apiUrl: 'https://api.track.toggl.com',
-  reportsUrl: 'https://api.track.toggl.com/reports/api/v2'
+  reportsUrl: 'https://api.track.toggl.com/reports'
 };
 
 