699c0b78bf7ac41f538ce30842ac56ffdbfd863c
rzfzr <capitao.rafa@gmail.com>
2024-04-25 23:45:20 -0300

    Serving all tags endpoints

--- tests/tags.test.js
+++ tests/tags.test.js
@@ -22,15 +22,37 @@ describe('Testing Tags', () => {
     togglClient.destroy();
   });
 
-  it('should get tags', async () => {
+  it('should get tags, create it, edit it, create it', async () => {
+    async function deleteTag(tagId) {
+      await togglClient.deleteTag(workspaceId, tagId)
+      await sleep(100)
+      const tagsAfterDelete = await togglClient.getTags(workspaceId)
+      expect(tagsAfterDelete).toBeInstanceOf(Array);
+    }
+    async function createTag() {
+      const tag = await togglClient.createTag(newTag.wid, newTag.name)
+      expect(tag).toHaveProperty('id');
+      return tag.id
+    }
+
+
     const tags = await togglClient.getTags(workspaceId)
     expect(tags).toBeInstanceOf(Array);
     expect(tags[0]).toHaveProperty('id');
-  })
 
-  it.only('should create a new tag', async () => {
-    const tag = await togglClient.createTag(newTag.name, newTag.wid)
-    expect(tag).toHaveProperty('id');
+
+    const tagToBeDeleted = tags.find(tag => tag.name === newTag.name)
+    if (tagToBeDeleted) {
+      await deleteTag(tagToBeDeleted.id)
+      await createTag()
+    } else {
+      const id = await createTag()
+
+      const updatedTag = await togglClient.updateTagName(workspaceId, id, 'Updated tag')
+      expect(updatedTag).toHaveProperty('name', 'Updated tag');
+      await deleteTag(id)
+    }
+
   })
 
 });
