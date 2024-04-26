df916925b1565ac104d29753ff8223868dcd67ca
rzfzr <capitao.rafa@gmail.com>
2024-04-26 13:33:26 -0300

    Creating invitation module

---
+++ tests/invitations.test.js
@@ -0,0 +1,34 @@
+'use strict';
+const TogglClient = require('../');
+require('dotenv').config()
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('Testing Invitations', () => {
+  let togglClient
+  const workspaceId = Number(process.env.WORKSPACE_ID)
+  const organizationId = Number(process.env.ORGANIZATION_ID)
+
+  beforeEach(() => {
+    togglClient = new TogglClient({ apiToken: process.env.API_TOKEN });
+  });
+
+  afterEach(() => {
+    togglClient.destroy();
+  });
+
+  it.only('should create a new organization invitation', async () => {
+    const emails = ['clicnet.com.br@gmail.com']
+    const workspaces = [{
+      workspace_id: workspaceId,
+    }]
+
+    const invitations = await togglClient.inviteUsers(organizationId, workspaces, emails)
+    expect(invitations).toHaveProperty('messages');
+
+  })
+
+});
+
